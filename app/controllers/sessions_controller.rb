c5e157bd9a62daa20b23327c8e77638db302230b
Olanrewaju Oduola <lanzzd191@gmail.com>
2024-01-28 13:57:52 +0100

    Add

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
        if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        render json: {token: user.token, user: user}, status: :ok
+        render json: {token: user.token, user: {name: user.username}}, status: :ok
        else
         render json: {error: "Invalid email or password"}, status: :unauthorized
        end
