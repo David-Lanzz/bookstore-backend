57dd398484113079275531cc13c76a53217cab24
Olanrewaju Oduola <lanzzd191@gmail.com>
2024-03-06 04:41:38 +0100

    Add validation to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :level
   has_many :books
+
+  validates :name presence: true
+  validates :level_id, presence: true
+  validates :alias, presence: true
 end
